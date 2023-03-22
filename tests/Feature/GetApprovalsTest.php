38f6a82e0f3f89b454018c8561de361432834cde
Bojan <bojan@talentdatalabs.com>
2023-03-22 10:59:57 +0100

    .

---
+++ tests/Feature/GetApprovalsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Approval;
+use App\Models\Job;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class GetApprovalsTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $user = User::factory()->nonApprover()->create([
+            'password' => bcrypt($password = 'password'),
+        ]);
+
+
+        $response = $this->actingAs($user)
+            ->get('/api/approval/');
+
+        $response->assertStatus(200);
+    }
+}
