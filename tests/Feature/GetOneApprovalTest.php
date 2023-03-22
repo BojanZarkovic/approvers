6903cc5b73d850152044e1e7d6d087368a4aa875
Bojan <bojan@talentdatalabs.com>
2023-03-22 10:57:42 +0100

    .

---
+++ tests/Feature/GetOneApprovalTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Approval;
+use App\Models\Job;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class GetOneApprovalTest extends TestCase
+{
+
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $user = User::factory()->nonApprover()->create([
+            'password' => bcrypt($password = 'password'),
+        ]);
+
+        $job = Job::factory()->trader()->create();
+
+        $approval = Approval::factory()->approved()->create([
+            'user_id' => $user->id,
+            'job_id' => $job->id
+        ]);
+
+
+        $response = $this->actingAs($user)
+            ->get('/api/approval/' . $approval->id);
+
+        $response->assertStatus(200);
+    }
+}
